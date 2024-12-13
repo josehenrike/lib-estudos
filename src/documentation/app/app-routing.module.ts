40c5c5325252e420404c40e7ab5ccee288f1a95f
Daniel <danielcarmo29@gmail.com>
2024-12-13 10:04:39 -0300

    tudo funcional por enquanto

--- src/documentation/app/app-routing.module.ts
+++ src/documentation/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
-import { MenuComponent } from '../../lib/components/menu/menu.component';
 import { NgModule } from '@angular/core';
 import { SearchComponent } from '../../lib/components/search/search.component';
 import { AppComponent } from './app.component';
+import { DropdownComponent } from '../../lib/components/dropdown/dropdown.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'search', component: SearchComponent },
+  { path: 'dropdown', component: DropdownComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/documentation/app/app.routes.ts
+++ src/documentation/app/app.routes.ts
@@ -3,8 +3,10 @@ import { MenuComponent } from '../../lib/components/menu/menu.component';
 import { NgModule } from '@angular/core';
 import { SearchComponent } from '../../lib/components/search/search.component';
 import { AppComponent } from './app.component';
+import { DropdownComponent } from '../../lib/components/dropdown/dropdown.component';
 
 export const routes: Routes = [
   { path: '', component: MenuComponent },
   { path: 'search', component: SearchComponent },
+  { path: 'dropdown', component: DropdownComponent },
 ];
