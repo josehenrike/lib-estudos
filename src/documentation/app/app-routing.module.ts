7386a63f798a3786e6cc6b64b2f0a8d41c333be4
Henrike <josehenrikejh572@gmail.com>
2024-12-13 13:34:19 -0300

    feat: edited button add and edit product

--- src/documentation/app/app-routing.module.ts
+++ src/documentation/app/app-routing.module.ts
@@ -1,16 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { SearchComponent } from '../../lib/components/search/search.component';
-import { AppComponent } from './app.component';
-import { DropdownComponent } from '../../lib/components/dropdown/dropdown.component';
+import { routes } from './app.routes';
 
-const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'search', component: SearchComponent },
-  { path: 'dropdown', component: DropdownComponent },
-];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/documentation/app/app.routes.ts
+++ src/documentation/app/app.routes.ts
@@ -1,8 +1,6 @@
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { MenuComponent } from '../../lib/components/menu/menu.component';
-import { NgModule } from '@angular/core';
 import { SearchComponent } from '../../lib/components/search/search.component';
-import { AppComponent } from './app.component';
 import { DropdownComponent } from '../../lib/components/dropdown/dropdown.component';
 
 export const routes: Routes = [
