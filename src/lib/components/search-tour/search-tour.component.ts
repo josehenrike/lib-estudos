e6de9a9a880be0306ceb93930fc755b973eb35e3
Daniel <danielcarmo29@gmail.com>
2025-01-08 10:37:24 -0300

    new componen

---
+++ src/lib/components/search-tour/search-tour.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-tour',
+  imports: [],
+  templateUrl: './search-tour.component.html',
+  styleUrl: './search-tour.component.scss'
+})
+export class SearchTourComponent {
+
+}
